7492af1301f8f796778aa5d568592134ca2f8228
邹江林 <jianglinzou@creditease.cn>
2019-07-10 17:05:20 +0800

    添加测试文件

--- src/main/java/com/lin/annotation/Name.java
+++ src/main/java/com/lin/annotation/Name.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.Mapping;
 import java.lang.annotation.*;
 
 /**
+ * 使用该注解可指定可连接的数据库名
+ * 注意：作用在接口上将不生效
  * @author jianglinzou
  * @date 2019/7/10 下午2:56
  */
-
-
 @Target({ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
@@ -18,5 +18,5 @@ public @interface Name {
 
     String name();
 
-    int order() default 0;
+    int order() default 0; //优先级，越大代表优先级越高
 }
