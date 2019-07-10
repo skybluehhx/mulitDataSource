db4662f77d4caeecb982a44776b26f027c4f442f
邹江林 <jianglinzou@creditease.cn>
2019-07-10 21:45:50 +0800

    添加测试类

---
+++ src/main/java/com/lin/dao/DataSourceTwoDao.java
@@ -0,0 +1,8 @@
+package com.lin.dao;
+
+/**
+ * @author jianglinzou
+ * @date 2019/7/10 下午9:35
+ */
+public class DataSourceTwoDao {
+}
---
+++ src/main/java/com/lin/server/DataSourceTwoServer.java
@@ -0,0 +1,8 @@
+package com.lin.server;
+
+/**
+ * @author jianglinzou
+ * @date 2019/7/10 下午9:39
+ */
+public class DataSourceTwoServer {
+}
