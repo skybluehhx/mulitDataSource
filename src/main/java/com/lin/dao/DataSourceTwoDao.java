592a42bf711f6cddabc4c539da20589d18bde5c5
邹江林 <jianglinzou@creditease.cn>
2019-07-10 21:46:50 +0800

    添加测试类

--- src/main/java/com/lin/dao/DataSourceTwoDao.java
+++ src/main/java/com/lin/dao/DataSourceTwoDao.java
@@ -1,8 +1,17 @@
 package com.lin.dao;
 
+import org.apache.ibatis.annotations.ResultType;
+import org.apache.ibatis.annotations.Select;
+
 /**
  * @author jianglinzou
  * @date 2019/7/10 下午9:35
  */
-public class DataSourceTwoDao {
+public interface DataSourceTwoDao {
+
+
+    @ResultType(String.class)
+    @Select("select name from datasource2 where id =#{0}")
+    public String test(int id);
+
 }
--- src/main/java/com/lin/server/DataSourceTwoServer.java
+++ src/main/java/com/lin/server/DataSourceTwoServer.java
@@ -1,8 +1,25 @@
 package com.lin.server;
 
+import com.lin.annotation.Name;
+import com.lin.dao.DataSourceTwoDao;
+import org.aspectj.lang.annotation.After;
+import org.springframework.beans.factory.annotation.Autowired;
+
 /**
  * @author jianglinzou
  * @date 2019/7/10 下午9:39
  */
 public class DataSourceTwoServer {
+
+    @Autowired
+    DataSourceTwoDao dataSourceTwoDao;
+
+    @Name(name = "dataSourceTwo", order = 15)
+    public String test(int id) {
+        String value = dataSourceTwoDao.test(id);
+        System.out.println("======value:" + value + " from dataSourceTwoServer=======");
+        return value;
+    }
+
+
 }
